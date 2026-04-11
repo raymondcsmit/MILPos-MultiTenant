35c15c8584d2fcb2be69d976cdc512c3e5bb88bf
raymondcsmit <waqarhabibmit@yahoo.com>
2026-04-11 11:35:29 +0500

    Update

--- SourceCode/SQLAPI/POS.Migrations.PostgreSQL/Migrations/20260411050655_OptimizationPostgreSQL.Designer.cs
+++ SourceCode/SQLAPI/POS.Migrations.PostgreSQL/Migrations/20260411050655_OptimizationPostgreSQL.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using POS.Domain;
@@ -11,9 +12,11 @@ using POS.Domain;
 namespace POS.Migrations.PostgreSQL.Migrations
 {
     [DbContext(typeof(POSDbContext))]
-    partial class POSDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260411050655_OptimizationPostgreSQL")]
+    partial class OptimizationPostgreSQL
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
